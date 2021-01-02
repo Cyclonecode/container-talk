6f8c9b9537bf44644e0cc7c370e28095959c3605
Krister Andersson <cyclonecode@gmail.com>
2021-01-02 05:32:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Example of two containers that can access each other
+
+This is achieved by setting up different network aliases for each container
+and then use this alias for access.
---
+++ index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('ok');
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`app listening on ${process.env.PORT}`);
+})
